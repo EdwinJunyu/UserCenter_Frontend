c1899f885f1d782b440f9035c0e19f3bbc639271
Edwin <junyuchen0907@gmail.com>
2024-12-26 04:05:12 -0800

    First submission -- 2024.12.26

---
+++ config/config.dev.ts
@@ -0,0 +1,14 @@
+// https://umijs.org/config/
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  plugins: [
+    'react-dev-inspector/plugins/umi/react-inspector',
+  ],
+
+  inspectorConfig: {
+    exclude: [],
+    babelPlugins: [],
+    babelOptions: {},
+  },
+});
